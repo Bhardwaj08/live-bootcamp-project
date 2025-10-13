64e33cdbb92aef7abbc4e7b96eb6e156309c438a
Bhardwaj08 <bhardwaj.miriyala@gmail.com>
2025-10-13 00:13:33 -0400

    [Sprint 1][Task 6] Implementing routes using TDD

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,7 +1,6 @@
 use std::error::Error;
-use axum::{serve::Serve, response::Html, routing::get, Router};
+use axum::{http::StatusCode, response::Html, routing::{get, post}, serve::Serve, Router, response::IntoResponse};
 use tower_http::services::ServeDir;
-
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -18,6 +17,9 @@ impl Application {
         // We don't need it at this point!
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
             .route("/hello", get( {
                 Html("<h1>Hello, World! Welcome to Axum!</h1>")
             }));
@@ -34,4 +36,16 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
